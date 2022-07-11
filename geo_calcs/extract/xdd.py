7d8643acd257b6c4c8d74a52973c4e59a750e7c1
Drew Heasman <45907989+drew026@users.noreply.github.com>
2022-07-11 17:12:05 -0230

    update on tdm

--- geo_calcs/extract/xdd.py
+++ geo_calcs/extract/xdd.py
@@ -95,7 +95,7 @@ def snippet_search(locations, keywords, headers={}):
         A dataframe of the snippet search results without duplicates and
         sorted by hits.
     """
-    snippets = "https://xdd.wisc.edu/api/snippets?"
+    snippets = "https://xdd.wisc.edu/api/snippets"
 
     locations = locations
     keywords = keywords
@@ -109,14 +109,16 @@ def snippet_search(locations, keywords, headers={}):
             search_term = search_term + keyword
             params = {
                 'term': search_term,
-                'fragment_limit': 500,
-                #    'full_results': True,
+                'fragment_limit': 5,
+                'article_limit': 50,
+#                'full_results': True,
                 'inclusive': True,
-                'clean': True,
+                'clean': True,                
                 }
             data = request_api(snippets, params, headers)
             temp = get_df_xDD(data)
-            temp['keywords'] = search_term
+            temp['location'] = location
+            temp['keyword'] = keyword
             df = df.append(temp)
 
     df = df.drop_duplicates(subset=['_gddid'])
@@ -125,6 +127,25 @@ def snippet_search(locations, keywords, headers={}):
 
     return df
 
+def get_term_list(df, col):
+    '''
+    Given a pandas Dataframe, returns a list.
+
+    Parameters
+    ----------
+    df : pandas Dataframe
+        
+    col : string
+        The name of t he column to be returned as a list
+
+    Returns
+    -------
+    term_list : List
+    '''
+    
+    term_list = df[col].tolist()
+    term_list = [x for x in term_list if str(x) != 'nan']
+    return term_list
 
 def extract_years(df, column):
     """ Function takes a dataframe and column with imbedded year information
@@ -168,3 +189,61 @@ def extract_authors(df, column):
     df['author_list'] = df[column].apply(lambda x: [word for word,pos in \
                     re.pos_tag(x.replace(',', '').split()) if pos == 'NNP'])
     return df
+
+def distance(s, w1, w2):
+    '''
+    Measures the proximity between two words.
+
+    Parameters
+    ----------
+    s : String
+        Raw string to parse for distance.
+    w1 : String
+        First word for comparison.
+    w2 : String 
+        Second word for comparison.
+
+    Returns
+    -------
+    Integer
+        Number of words between the two comparison words.
+
+    '''
+     
+    if w1 == w2 :
+       return 0
+ 
+    # get individual words in a list
+    words = s.split(" ")
+    n = len(words)
+ 
+    # assume total length of the string as
+    # minimum distance
+    min_dist = n+1
+ 
+    # Find the first occurrence of any of the two
+    # numbers (w1 or w2) and store the index of
+    # this occurrence in prev
+    for i in range(n):
+         
+        if words[i] == w1 or words[i] == w2:
+            prev = i
+            break
+ 
+    # Traverse after the first occurrence
+    while i < n:
+        if words[i] == w1 or words[i] == w2:
+ 
+            # If the current element matches with
+            # any of the two then check if current
+            # element and prev element are different
+            # Also check if this value is smaller than
+            # minimum distance so far
+            if words[prev] != words[i] and (i - prev) < min_dist :
+                min_dist = i - prev - 1
+                prev = i
+            else:
+                prev = i
+        i += 1       
+ 
+    return min_dist
